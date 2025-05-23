3783006a8ba5fdb938c393fb6483244dbc22fffb
Attanasio-Edoardo <attanasio.edoardo@istitutoagnelli.it>
2025-05-23 12:37:54 +0200

    fixed about

--- docs/_app/immutable/entry/start.CTk6-ALa.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/jQHNG1Ac.js";export{o as load_css,r as start};
---
+++ docs/_app/immutable/nodes/3.Buu5scH3.js
@@ -0,0 +1,1 @@
+import{t as a,a as p}from"../chunks/D-UNy9-O.js";import"../chunks/BIUmOpKY.js";import{n as e}from"../chunks/DUijmhlk.js";function i(t){e();var o=a("ciao");p(t,o)}export{i as component};
