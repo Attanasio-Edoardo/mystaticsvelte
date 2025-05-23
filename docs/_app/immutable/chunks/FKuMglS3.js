138208dbe141af25198e3a4e205d1a9dc61f92fc
Attanasio-Edoardo <attanasio.edoardo@istitutoagnelli.it>
2025-05-23 12:40:10 +0200

    fixed about fr

---
+++ docs/_app/immutable/chunks/FKuMglS3.js
@@ -0,0 +1,1 @@
+var s;const e=((s=globalThis.__sveltekit_980krc)==null?void 0:s.base)??"/mystaticsvelte";var t;const a=((t=globalThis.__sveltekit_980krc)==null?void 0:t.assets)??e;export{a,e as b};
--- docs/_app/immutable/entry/start.BsqYB7lw.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/r2SqBE3Z.js";export{o as load_css,r as start};
